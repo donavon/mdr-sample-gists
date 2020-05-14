aadcef1866f6aa7a473b39948f0b8e9e73ea1b58
donavon <github@donavon.com>
2020-05-13 21:28:07 -0400

    fix dependency

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { ReactQueryDevtools } from 'react-query-devtools';
 import Routes from '../routes';
 
 export default function App() {
-  return (
-    <>
-      <Routes />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </>
-  );
+  return <Routes />;
 }
